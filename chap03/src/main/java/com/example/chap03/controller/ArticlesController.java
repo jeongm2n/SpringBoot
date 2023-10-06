5f2c22715c634871a47c72494b34027cc5aff812
서정민 <wjdals8625@gmail.com>
2023-10-06 15:27:36 +0900

    단일 데이터 조회하기

--- chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
+++ chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
@@ -6,7 +6,9 @@ import com.example.chap03.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Controller
@@ -29,5 +31,19 @@ public class ArticlesController {
         Article saved = articleRepository.save(article);
         log.info(article.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 }
