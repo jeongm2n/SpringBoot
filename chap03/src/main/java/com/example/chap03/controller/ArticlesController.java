f62ec69208932f23bbd9fac7f8abc6555b2c53aa
서정민 <wjdals8625@gmail.com>
2023-10-06 16:07:18 +0900

    모든 데이터 조회하기

--- chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
+++ chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 public class ArticlesController {
@@ -30,7 +32,7 @@ public class ArticlesController {
 
         Article saved = articleRepository.save(article);
         log.info(article.toString());
-        return "";
+        return "/articles/new";
     }
 
     @GetMapping("/articles/{id}")
@@ -45,5 +47,17 @@ public class ArticlesController {
 
         //3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList",articleEntityList);
+
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- chap03/src/main/java/com/example/chap03/repository/ArticleRepository.java
+++ chap03/src/main/java/com/example/chap03/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.example.chap03.repository;
 import com.example.chap03.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
