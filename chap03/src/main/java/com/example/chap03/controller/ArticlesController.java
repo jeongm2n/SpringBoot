942260d97ed590b476261e6959f271b72a49c9ba
서정민 <wjdals8625@gmail.com>
2023-10-10 18:02:15 +0900

    게시글 수정 완료

--- chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
+++ chap03/src/main/java/com/example/chap03/controller/ArticlesController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -66,5 +67,16 @@ public class ArticlesController {
         Article articleEntity = articleRepository.findById(id).orElse(null);
         model.addAttribute("article", articleEntity);
         return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticlesDTO artDTO){
+        Article articleEntity = artDTO.toEntity();
+
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/" + articleEntity.getId();
     }
 }
--- chap03/src/main/java/com/example/chap03/entity/Article.java
+++ chap03/src/main/java/com/example/chap03/entity/Article.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
