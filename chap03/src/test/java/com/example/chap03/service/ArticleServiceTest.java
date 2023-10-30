2d6b689da3592a026caf041a27eb157df4b80be6
서정민 <wjdals8625@gmail.com>
2023-10-30 15:20:11 +0900

    Test, 예상 데이터 작성

--- chap03/src/test/java/com/example/chap03/service/ArticleServiceTest.java
+++ chap03/src/test/java/com/example/chap03/service/ArticleServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -16,6 +18,11 @@ class ArticleServiceTest {
     @Test
     void index() {
         //1. 예상 데이터
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a,b,c));
+
         //2. 실제 데이터
         List<Article> articles = articleService.index();
         //3. 비교 및 검증
